009545a80ba8c2dae139c457e67e1ad6fd693484
Support <Support@WEMA-TRAINING-S>
2022-08-25 10:26:10 +0100

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using BankApp.Models.View_Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BankApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        public ActionResult Register(Customers customers)
+        {
+            return View();
+        }
+    }
+    
+}
---
+++ Models/View Model/Customers.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BankApp.Models.View_Model
+{
+    public class Customers
+    {
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public DateTime DOB { get; set; }
+    }
+}
